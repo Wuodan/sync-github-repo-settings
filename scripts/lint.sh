3c48d632f7a8dd84bdd49239ea28e0c29f30fbe4
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-08-07 15:44:32 +0200

    Update lint.sh script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 repo_root=$(git rev-parse --show-toplevel)
 cd "$repo_root"
 
+# YAML
 yamllint .
+
+# Markdown
 pymarkdown \
   --config .pymarkdown.json scan \
   --recurse \
+  --exclude '**/.venv*/**' \
   .
